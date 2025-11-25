34b0f8445685d6e7cb85c108057a40ba424fee1b
Thibault Feraux <thibault.feraux@usewhale.io>
2025-11-25 09:43:31 +0100

    add feature 1 on main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
     print("Hello from git-rebase-demo-test!")
     print("Welcome to my main branch")
+    feature_1()
+
+def feature_1():
+    print("This is feature 1 in main")
 
 
 if __name__ == "__main__":
